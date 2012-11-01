2cf771d2391e308675d5d2d3eb0276184af5e7b7
Steve Tuckner <steve@open-orgsolutions.com>
2012-11-01 01:32:50 -0500

    added files

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_many :tasks
+
+  def to_s
+    self.name
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskItRails::Application.config.secret_token = '278301e2012a9de36c0f80ca99e84d7a189448cdd36b962297621cb2e4383fee9edc612e04d0ce1311079eb5da3c059f4722075c9287b96078f404b4b49c230a'
